99d94887fcd574efa5a0c9a948090ef18654cd84
Callam Williams <lhocdesign@gmail.com>
2017-06-30 12:35:38 +0100

    [theme] - removed extra js used for testing eslint

--- httpdocs/callam-williams/themes/callam-williams/assets/src/js/classes/PropForms/index.js
+++
@@ -1,7 +0,0 @@
-import PropForms from './PropForms/PropForms';
-import SlickForms from './SlickForms/SlickForms';
-
-window.SlickForms = SlickForms;
-window.PropForms = PropForms;
-
-export { PropForms, SlickForms }
