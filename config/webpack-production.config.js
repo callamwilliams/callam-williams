3770dcdd0aa69f0c143736f8df98fd73c8444df5
Callam Williams <lhocdesign@gmail.com>
2017-06-30 12:36:52 +0100

    [config] - added ModuleConcatenationPlugin to webpack production task

--- config/webpack-production.config.js
+++ config/webpack-production.config.js
@@ -3,6 +3,7 @@ const WebpackConfig = require('webpack-config').default;
 
 module.exports = new WebpackConfig().extend('./config/webpack-base.config.js').merge({
     plugins: [
+        new webpack.optimize.ModuleConcatenationPlugin(),
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('production')
