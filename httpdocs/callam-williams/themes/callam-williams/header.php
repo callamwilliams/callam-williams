205ce69ff9bbd1f1ee6c48dd2e3e29cd0d103f5c
Callam Williams <lhocdesign@gmail.com>
2017-07-20 17:27:01 +0100

    [theme] - fixed analytics

--- httpdocs/callam-williams/themes/callam-williams/header.php
+++ httpdocs/callam-williams/themes/callam-williams/header.php
@@ -34,30 +34,26 @@
 	<meta name="theme-color" content="#fecb77">
 	<base href="<?= home_url(); ?>">
 
-	<? if ( PM::get_option( 'site', 'analytics' ) ) : ?>
+	<? $analytics = explode( "\n", 'UA-XXXXX-X' ); ?>
 
-		<? $analytics = explode( "\n", PM::get_option( 'site', 'analytics' ) ); ?>
-
-		<script>
-            // Google Analytics
-            window.gaKey = [];
-            (function (i, s, o, g, r, a, m) {
-                i['GoogleAnalyticsObject'] = r;
-                i[r] = i[r] || function () {
-                        (i[r].q = i[r].q || []).push(arguments)
-                    }, i[r].l = 1 * new Date();
-                a = s.createElement(o), m = s.getElementsByTagName(o)[0];
-                a.async = 1;
-                a.src = g;
-                m.parentNode.insertBefore(a, m)
-            })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-            <? foreach($analytics as $key => $code): ?>window.gaKey.push('<?= $key ?>');
-            ga('create', '<?= trim( $code ) ?>', 'auto', '<?= $key ?>');
-            ga('<?= $key ?>.send', 'pageview',<?= ( $page == "page-not-found" ) ? "'/404/?url='+document.location.pathname+document.location.search+'&ref='+document.referrer" : "document.location.pathname" ?>);
-			<? endforeach ?>
-		</script>
-
-	<? endif; ?>
+	<script>
+		// Google Analytics
+		window.gaKey = [];
+		(function (i, s, o, g, r, a, m) {
+			i['GoogleAnalyticsObject'] = r;
+			i[r] = i[r] || function () {
+				(i[r].q = i[r].q || []).push(arguments)
+			}, i[r].l = 1 * new Date();
+			a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+			a.async = 1;
+			a.src = g;
+			m.parentNode.insertBefore(a, m)
+		})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+		<? foreach($analytics as $key => $code): ?>window.gaKey.push('<?= $key ?>');
+		ga('create', '<?= trim( $code ) ?>', 'auto', '<?= $key ?>');
+		ga('<?= $key?>.send', 'pageview'<?= ( is_404() ) ? ",'/404/?url=' + document.location.pathname + document.location.search + '&ref=' + document.referrer" : "" ?>);
+		<? endforeach ?>
+	</script>
 
 	<?php wp_head(); ?>
 
