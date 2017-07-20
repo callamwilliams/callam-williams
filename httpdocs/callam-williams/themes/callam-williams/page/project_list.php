77b502e9f0f6e0b7dfc31f39e52381c4ee49b4f6
Callam Williams <lhocdesign@gmail.com>
2017-07-20 17:27:15 +0100

    [theme] - array chunk'd the projects list

--- httpdocs/callam-williams/themes/callam-williams/page/project_list.php
+++ httpdocs/callam-williams/themes/callam-williams/page/project_list.php
@@ -2,26 +2,53 @@
 
 $args = [
 	'post_type'   => 'project',
-	'orderby'     => 'date',
+	'orderby'     => 'meta',
 	'order'       => 'DESC',
 	'post_status' => 'publish'
 ];
 
 $loop = new WP_Query( $args );
-$i    = 0;
-while ( $loop->have_posts() ) : $loop->the_post();
-	$i ++;
-	?>
 
-	<article class="">
-		<a class="" href="<? the_permalink(); ?>">
-			<header>
-				<h1 class="">
-					<?php the_title(); ?>
-				</h1>
-			</header>
-		</a>
-	</article>
-<?php endwhile; ?>
+?>
+
+<? foreach ( array_chunk( $loop->posts, 3, false ) as $article ) : ?>
+	<? if ( isset( $article[0] ) ): ?>
+		<article class="">
+			<a class="" href="<?= get_post_permalink( $article[0]->ID ); ?>">
+				<header>
+					<h1 class="">
+						<?= $article[0]->post_title; ?>
+					</h1>
+				</header>
+			</a>
+		</article>
+	<? endif; ?>
+
+	<? if ( isset( $article[1] ) ): ?>
+		<article class="">
+			<a class="" href="<?= get_post_permalink( $article[1]->ID ); ?>">
+				<header>
+					<h1 class="">
+						<?= $article[1]->post_title; ?>
+					</h1>
+				</header>
+			</a>
+		</article>
+	<? endif; ?>
+
+	<? if ( isset( $article[2] ) ): ?>
+		<article class="">
+			<a class="" href="<?= get_post_permalink( $article[2]->ID ); ?>">
+				<header>
+					<h1 class="">
+						<?= $article[2]->post_title; ?>
+					</h1>
+				</header>
+			</a>
+		</article>
+	<? endif; ?>
+<? endforeach; ?>
+
+
 
 <?php wp_reset_postdata(); ?>
