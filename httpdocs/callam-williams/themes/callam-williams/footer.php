b39a1c351bc1a30d07adf3835d1d341d3c1febd8
Callam Williams <lhocdesign@gmail.com>
2017-07-23 11:35:35 +0100

    [theme] - footer and svgs

--- httpdocs/callam-williams/themes/callam-williams/footer.php
+++ httpdocs/callam-williams/themes/callam-williams/footer.php
@@ -15,7 +15,17 @@
 
 
 <footer id="footer" class="footer">
-
+	<div class="footer__links">
+		<a class="footer__link" href="//www.github.com/callamwilliams">
+			<span class="icon-github"></span>/callamwilliams
+		</a>
+		<a class="footer__link" href="https://stackoverflow.com/users/822671/callam">
+			<span class="icon-stack-overflow"></span>/callamwilliams
+		</a>
+		<a class="footer__link" href="//www.twitter.com/callamwilliams">
+			<span class="icon-twitter"></span>@callamwilliams
+		</a>
+	</div>
 </footer>
 
 <?php wp_footer(); ?>
